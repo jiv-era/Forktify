2faf99a8c3c1db2778bd6344dea19cebd4d573d8
Jordan R <jaygerzero@gmail.com>
2020-01-28 23:18:39 -0500

    Test MVC

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
-// Global app controller
-import num from './test';
-const x = 23;
-console.log(`I imported ${num} from another module called test.js! Variable x is ${x}`);
+import string from './models/Search';
+// import { add, multiply, ID } from './views/searchView';
+import * as searchView from './views/searchView';
+
+console.log(`Using imported functions! ${searchView.add(ID, 2)} ${searchView.multiply(ID, 2)} ${searchView.ID, 2} ${str}`);
+
