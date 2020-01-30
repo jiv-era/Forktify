87f654d56497b4594fa6034bcaca634bfffda667
Jordan R <jaygerzero@gmail.com>
2020-01-30 17:52:26 -0500

    Building Search View p.2

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -4,11 +4,34 @@ export const getInput = () => elements.searchInput.value
 
 export const clearInput = () => {
     elements.searchInput.value = '';
-}
+};
 
 export const clearResults = () => {
     elements.searchResList.innerHTML = '';
-}
+};
+/*
+// 'Pasta with tomato and spinach'
+acc: 0 / acc + curr.length = 5 / newTitle = ['Pasta']
+acc: 5 / acc + curr.length = 9 / newTitle = ['Pasta', 'with']
+acc: 9 / acc + curr.length = 15 / newTitle = ['Pasta','with', 'tomato']
+acc: 15 / acc + curr.length = 18 / newTitle = ['Pasta','with', 'tomato']
+acc: 18 / acc + curr.length = 24 / newTitle = ['Pasta','with', 'tomato']
+*/
+const limitRecipeTitle = (title, limit = 17) => {
+    const newTitle = [];
+    if (title.length > limit) {
+        title.split(' ').reduce((acc, cur) => {
+            if(acc + cur.length <= limit) {
+                newTitle.push(cur);
+            }
+            return acc + cur.length;
+        }, 0);
+
+        // return the result
+        return `${newTitle.join(' ')}...`;
+    }
+    return title;
+};
 
 const renderRecipe = recipe => {
     const markup = `
@@ -18,14 +41,13 @@ const renderRecipe = recipe => {
                 <img src="${recipe.image_url}" alt="${recipe.title}">
             </figure>
             <div class="results__data">
-                <h4 class="results__name">${recipe.title}</h4>
+                <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
                 <p class="results__author">${recipe.publisher}</p>
             </div>
         </a>
     </li>`;
     elements.searchResList.insertAdjacentHTML('beforeend', markup);
-
-}
+};
 
 export const renderResults = recipes => {
     console.log(recipes);
