e4f7bda7e61d2e1cafe44d215bae94bec15119ab
Jordan R <jaygerzero@gmail.com>
2020-02-09 20:36:42 -0500

    Recipe model pt.3

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -33,4 +33,21 @@ export default class Recipe {
         this.servings = 4;
     }
     
+    parseIngredients() {
+        const unitsLong = ['tablespoons', 'tablespoon', 'ounce', 'ounces', 'teaspoon', 'teaspoons', 'cups', 'pounds'];
+        const unitsShort = ['tbsp', 'tbsp', 'oz', 'oz', 'tsp', 'cup', 'pound'];
+
+        const newIngredients = this.ingredients.map(el => {
+            // 1) Uniform units
+            let ingredient = el.toLowerCase();
+            unitsLong.forEach((unit, i) => {
+                ingredient = ingredient.replace(unit, unitsShort[i]);
+            });
+
+            // 2) Remove parentheses
+            ingredient = ingredient.replace(/ *\([^)]*\) */g, "");
+            // 3) Parse ingredients into count, unit ad ingredient 
+        });
+        this.ingredients = newIngredients;
+    }
 }
